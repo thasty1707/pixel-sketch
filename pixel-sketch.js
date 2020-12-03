261ca696b81a1409b8918f83333f7b01de8546c7
thasty1707 <tim.hasty@gmail.com>
2020-12-03 17:14:26 -0600

    Create empty functions; add style to tools div and child elements

---
+++ pixel-sketch.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('container');
+const clearBtn = document.getElementById('clear');
+
+function makeGrid(){
+
+};
+
+function blackCells(){
+
+};
+
+function colorfulCells(){
+
+};
+
+function darkenCells(){
+
+};
+
+function clearGrid(){
+
+};
