72f02c20cff37a7e1fa10c09f65bd75225113efa
thasty1707 <tim.hasty@gmail.com>
2020-12-03 17:24:42 -0600

    Add variables and fillCells function in js

--- pixel-sketch.js
+++ pixel-sketch.js
@@ -1,5 +1,9 @@
 const container = document.getElementById('container');
 const clearBtn = document.getElementById('clear');
+const pixelCount = document.getElementById('sizeBar');
+const blackPixels = document.getElementById('toBlack');
+const randomColors = document.getElementById('colorful');
+const greyScale = document.getElementById('darken');
 
 function makeGrid(){
 
@@ -20,3 +24,7 @@ function darkenCells(){
 function clearGrid(){
 
 };
+
+function fillCells(){
+
+}
