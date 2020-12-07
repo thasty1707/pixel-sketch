658fd385591aadff516a77aadbbb66eba60c6a80
thasty1707 <tim.hasty@gmail.com>
2020-12-07 10:35:57 -0600

    Function did not work; removed; start new attempt

--- pixel-sketch.js
+++ pixel-sketch.js
@@ -1,27 +1,18 @@
-//const container = document.getElementById('container');
+const container = document.getElementById('container');
+
+//const pixelCount = document.getElementById('sizeBar');
+
 const clearBtn = document.getElementById('clear');
-const pixelCount = document.getElementById('sizeBar');
 const blackPixels = document.getElementById('toBlack');
 const randomColors = document.getElementById('colorful');
 const greyScale = document.getElementById('darken');
 
-function gridLayout(height, width){
-    const container = document.getElementById('container');
-    container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${width}, 1fr)`;
-};
-
-const fillGrid = (x, y, blocksize, numOfBlocks, color) => {
-    const container = document.getElementById('container');
-    const test = Array(num_of_blocks).keys()
-        for(var i of test){
-            let markup = `<div id="card ${i}"
-                                style = "grid-column: ${Math.floor(Math.random()*= * y) + 1} / span ${blcokSize};
-                                background-clor: ${color};
-                                border: 1px black solid;">${1}</div>`;
-            container.innherHTML += markup;
-        };
-}
+//Can this be helpful?
+//function gridLayout(height, width){
+    //const container = document.getElementById('container');
+    //container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+    //container.style.gridTemplateRows = `repeat(${width}, 1fr)`;
+//};
 
 function blackCells(){
 
