6c418d3d4747d9f1478294f887613f63a81f6fd3
thasty1707 <tim.hasty@gmail.com>
2020-12-10 10:53:04 -0600

    Removed most changeGrid and sketchPadSize fnctns; edit sizeTools style; add New Grid button

--- pixel-sketch.js
+++ pixel-sketch.js
@@ -6,7 +6,7 @@ const clearBtn = document.getElementById('clear');
 const blackPixels = document.getElementById('toBlack');
 const randomColors = document.getElementById('colorful');
 const greyScale = document.getElementById('darken');
-const change = document.getElementById('submit');
+const change = document.getElementById('newGrid');
 
 let slider = document.getElementById('gridSize');
 let output = document.getElementById('pixelNumber');
@@ -15,8 +15,8 @@ output.innerHTML = slider.value;
 slider.oninput = function(){
     output.innerHTML = this.value;
 };
-//functions to fill container
 
+//functions to fill container
 function defaultGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -28,26 +28,12 @@ function defaultGrid(rows, cols){
 };
 
 function changeGrid(){
-    let cellNum = slider.value;
-    
-    for(c = 0; c < cellNum * cellNum; c++){
-        let cell = document.createElement("div");
-        //container.style.setProperty('--grid-rows', cellNum);
-        //container.style.setProperty('--grid-cols', cellNum);
-        container.style.gridTemplateColumns = `repat(${cellNum}, 1fr)`;
-        container.style.gridTemplateRows = `repat(${cellNum}, 1fr)`;
-        //container.appendChild(cell).className = "cell";
-        container.insertAdjacentElement('beforeend',cell);
-    };
 
-    
 };
 
-//function sketchPadSize(){
-    //let boxes = container.querySelectorAll('div');
-    //boxes.forEach(boxes => boxes.remove());
-    //changeGrid(gridSize, gridSize);
-//};
+function sketchPadSize(){
+
+};
 
 function blackCells(){
 
@@ -72,6 +58,4 @@ function fillCells(){
 //load with default size
 defaultGrid(16,16);
 
-//container.addEventListener('onload',defaultGrid(16, 16));
-slider.addEventListener('input',changeGrid());
-//submit.addEventListener('onclick',sketchPadSize());
+//change.addEventListener('click',changeGrid());
